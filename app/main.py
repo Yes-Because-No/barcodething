f32e95c57a00bd91c3f498b46a47aa5c16811a05
Yes-Because-No <alex.tichindelean@gmail.com>
2025-06-28 21:17:13 -0400

    Basic fastapi camera setup

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from services.scanner import Camera, CameraCallbacks
+import cv2
+import asyncio
+import numpy as np
+
+cam = Camera(0)
+
+app = FastAPI()
+        
+
+@app.get("/")
+def main():
+    def combo(frame):
+        ret, decoded, img = CameraCallbacks.scan_for_barcode(frame)
+        if ret:
+            print(decoded.data.decode("utf-8"))
+            img = np.full((480,640, 3), (0,255,0))
+        return CameraCallbacks.to_web_streamable(img)
+        
+    return StreamingResponse(cam.gen_frames(combo), media_type="multipart/x-mixed-replace; boundary=frame")
+
+
